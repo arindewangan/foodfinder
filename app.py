261bf23131a30e1f4fa393a3a672385d960bbd8f
Arin Dewangan <arinaws59@gmail.com>
2023-03-01 16:45:44 +0530

    final commit

--- app.py
+++ app.py
@@ -3,23 +3,30 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.ensemble import RandomForestClassifier
 
-# Load the dataset
-recipes = pd.read_csv('food.csv')
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-# Vectorize the ingredients
-vectorizer = CountVectorizer()
-X = vectorizer.fit_transform(recipes['ingredients'])
+# Load dataset
+data = pd.read_csv("food.csv")
 
-# Train the model
-y = recipes['dish']
-model = RandomForestClassifier()
-model.fit(X, y)
+# Vectorize ingredients
+vectorizer = CountVectorizer(token_pattern=r'\b\w+\b')
+X = vectorizer.fit_transform(data['ingredients'])
 
-# Predict food dishes based on input ingredients
-def predict_dish(ingredients):
-    X_test = vectorizer.transform([ingredients])
-    y_pred = model.predict(X_test)
-    return y_pred[0]
+# Deploy model
+def predict_dishes(ingredients, threshold=0.6):
+    # Vectorize input ingredients
+    X_input = vectorizer.transform([ingredients])
+
+    # Compute cosine similarity between input and all recipes
+    similarities = cosine_similarity(X_input, X).flatten()
+
+    # Get indices of recipes with similarity above threshold
+    indices = [i for i, sim in enumerate(similarities) if sim > threshold]
+
+    # Return the dish names of recipes with similarity above threshold
+    return list(data.loc[indices]['dish'])
 
 
 # Frontend
@@ -32,9 +39,9 @@ def index():
 @app.route('/get', methods=['POST'])
 def get():
     ingredients = request.form['ingredients']
-    predicted_dish = predict_dish(ingredients)
+    predicted_dishes = predict_dishes(ingredients)
     ing = ingredients.split(',')
-    return render_template('result.html', predicted_dish=predicted_dish,ingredients=ingredients,ing=ing,len = len(ing))
+    return render_template('result.html', predicted_dishes=predicted_dishes,ingredients=ingredients,ing=ing,len = len(ing))
 
 if __name__ == '__main__':
     app.run(debug=True)
